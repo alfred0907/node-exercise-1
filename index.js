ff94e8b7af83057bf157ad70777487c9818ead12
John Alfred Velasco <113877494+alfred0907@users.noreply.github.com>
2022-10-13 07:45:11 +0800

    Implement file manipulation

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const { readFile } = require('node:fs')
+const { readFile, writeFile } = require('node:fs')
 
-readFile(process.argv[2], 'utf8', (err, data) => {
-  if (err) {
-    console.log(err.message)
-  } else {
-    console.log(data.split('\n').map((element, index) => `${index + 1}: ${element}`).join('\n'))
-  }
-})
+if (process.argv[2] === undefined) {
+  console.log('Requires an argument of path.')
+} else if (process.argv[3] === undefined) {
+  console.log('Second argument is required.')
+} else if (process.argv[4] !== undefined && process.argv[4] !== '-n' && process.argv[4] !== '-y') {
+  console.log(`${process.argv[4]} is not a valid argument! (Expected '-n', '-y' or undefined)`)
+} else {
+  readFile(process.argv[2], 'utf8', (err, data) => {
+    if (err) {
+      console.log(err.message)
+    } else {
+      writeFile(process.argv[3], data.split('\n').map((element, index) => `${index + 1}: ${element}`).join('\n'), { flag: process.argv[4] === undefined || process.argv[4] === '-n' ? 'wx' : 'w' }, (errWriteFile) => {
+        if (errWriteFile) {
+          console.log(errWriteFile.message)
+        }
+      })
+    }
+  })
+}
